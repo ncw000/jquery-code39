de4796608845e9dd70d55b607005df676c0c0087
Nathan Weir <ncw000@gmail.com>
2014-05-20 19:53:18 -0400

    Added minor, untested error handling. Wrapped all of the plugin code into the code39 function, so the plugin can be used more than once per page load without breaking. Also added a canvas clear to white upon drawing.

--- jquery-code39.js
+++ jquery-code39.js
@@ -29,131 +29,144 @@
 */
 
 (function($) {
-
-    // The char table for which code39 bars to draw for each of the 42 possible characters, as well
-    // as the start/end delimeter code character of "*".
-    //
-    //	b = narrow black
-    //	B = wide black
-    //	w = narrow white
-    //	W = wide white
-    var charTable = {
-        "0": "bwbWBwBwb",
-        "1": "BwbWbwbwB",
-        "2": "bwBWbwbwB",
-        "3": "BwBWbwbwb",
-        "4": "bwbWBwbwB",
-        "5": "BwbWBwbwb",
-        "6": "bwBWBwbwb",
-        "7": "bwbWbwBwB",
-        "8": "BwbWbwBwb",
-        "9": "bwBWbwBwb",
-        "A": "BwbwbWbwB",
-        "B": "bwBwbWbwB",
-        "C": "BwBwbWbwb",
-        "D": "bwbwBWbwB",
-        "E": "BwbwBWbwb",
-        "F": "bwBwBWbwb",
-        "G": "bwbwbWBwB",
-        "H": "BwbwbWBwb",
-        "I": "bwBwbWBwb",
-        "J": "bwbwBWBwb",
-        "K": "BwbwbwbWB",
-        "L": "bwBwbwbWB",
-        "M": "BwBwbwbWb",
-        "N": "bwbwBwbWB",
-        "O": "BwbwBwbWb",
-        "P": "bwBwBwbWb",
-        "Q": "bwbwbwBWB",
-        "R": "BwbwbwBWb",
-        "S": "bwBwbwBWb",
-        "T": "bwbwBwBWb",
-        "U": "BWbwbwbwB",
-        "V": "bWBwbwbwB",
-        "W": "BWBwbwbwb",
-        "X": "bWbwBwbwB",
-        "Y": "BWbwBwbwb",
-        "Z": "bWBwBwbwb",
-        "-": "bWbwbwBwB",
-        ".": "BWbwbwBwb",
-        " ": "bWBwbwBwb",
-        "$": "bWbWbWbwb",
-        "/": "bWbWbwbWb",
-        "+": "bWbwbWbWb",
-        "%": "bwbWbWbWb",
-        "*": "bWbwBwBwb"
-    };
-
-    // I'm using module (kinda global) level fields here. Go ahead, crucify me.
-
-    // Width/height of the canvas
-    var cWidth, cHeight;
-    // Needs a better name - the index (from the left) in 
-    var leftIndex = 0;
-
-    // The width, in pixels, of narrow & wide columns drawn onto this canvas.
-    // Should be rounded to the nearest int when set, depending on the size of the canvas.
-    var narrowWidth = 0;
-    var wideWidth = 0;
-
-    // The # of pixels from the left at which the drawing functions are pointing; this is
-    // where the next drawing operation will begin drawing a column. Updated whenever drawing
-    // occurs.
-    var curPixelIndex = 0;
-
-    // Draw the black/white bars for the given character
-    var drawCode = function(context, codeChar) {
-        // Get the sequence of wide, narrow, black, and white bar chars for this char
-        // Will be a sequence of B, b, W, & w, from the charTable
-        // NOTE: This *will* crash if you give it a char not in the table. Fix later.
-        var charBarSequence = charTable[codeChar];
-
-        // Draw a bar for each bar type character, advancing the curPixelIndex appropriately
-        for (var i = 0; i < charBarSequence.length; i++) {
-            drawBar(context, charBarSequence[i]);
-        };
-    };
-
-    // Draw the correct style of bar at the currentPixelIndex
-    var drawBar = function(context, typeChar) {
-        var fillCode = ""; // Black or white, set below
-        var fillWidth = 0; // The val of narrowWidth or wideWidth above, but set below
-
-        switch (typeChar) {
-            case "b":
-                fillWidth = narrowWidth;
-                fillCode = "black";
-                break;
-            case "B":
-                fillWidth = wideWidth;
-                fillCode = "black";
-                break;
-            case "w":
-                fillWidth = narrowWidth;
-                fillCode = "white";
-                break;
-            case "W":
-                fillWidth = wideWidth;
-                fillCode = "white";
-                break;
-        }
-
-        context.fillStyle = fillCode;
-        context.fillRect(curPixelIndex, 0, fillWidth, cHeight);
-
-        // Advance the curPixelIndex
-        curPixelIndex += fillWidth;
-    }
-
-    // Draw the code for a "*", the start and end code, meant to be placed at the beginning and
-    // end of the entire sequence.
-    var drawDelimeterCode = function(context) {
-        // Just a silly wrapper around drawCode()
-        drawCode(context, "*");
-    };
-
     // 'dataString' is the data to be encoded in code39
     $.fn.code39 = function(dataString) {
+        // The char table for which code39 bars to draw for each of the 42 possible characters, as well
+        // as the start/end delimeter code character of "*".
+        //
+        //	b = narrow black
+        //	B = wide black
+        //	w = narrow white
+        //	W = wide white
+        var charTable = {
+            "0": "bwbWBwBwb",
+            "1": "BwbWbwbwB",
+            "2": "bwBWbwbwB",
+            "3": "BwBWbwbwb",
+            "4": "bwbWBwbwB",
+            "5": "BwbWBwbwb",
+            "6": "bwBWBwbwb",
+            "7": "bwbWbwBwB",
+            "8": "BwbWbwBwb",
+            "9": "bwBWbwBwb",
+            "A": "BwbwbWbwB",
+            "B": "bwBwbWbwB",
+            "C": "BwBwbWbwb",
+            "D": "bwbwBWbwB",
+            "E": "BwbwBWbwb",
+            "F": "bwBwBWbwb",
+            "G": "bwbwbWBwB",
+            "H": "BwbwbWBwb",
+            "I": "bwBwbWBwb",
+            "J": "bwbwBWBwb",
+            "K": "BwbwbwbWB",
+            "L": "bwBwbwbWB",
+            "M": "BwBwbwbWb",
+            "N": "bwbwBwbWB",
+            "O": "BwbwBwbWb",
+            "P": "bwBwBwbWb",
+            "Q": "bwbwbwBWB",
+            "R": "BwbwbwBWb",
+            "S": "bwBwbwBWb",
+            "T": "bwbwBwBWb",
+            "U": "BWbwbwbwB",
+            "V": "bWBwbwbwB",
+            "W": "BWBwbwbwb",
+            "X": "bWbwBwbwB",
+            "Y": "BWbwBwbwb",
+            "Z": "bWBwBwbwb",
+            "-": "bWbwbwBwB",
+            ".": "BWbwbwBwb",
+            " ": "bWBwbwBwb",
+            "$": "bWbWbWbwb",
+            "/": "bWbWbwbWb",
+            "+": "bWbwbWbWb",
+            "%": "bwbWbWbWb",
+            "*": "bWbwBwBwb"
+        };
+
+        // I'm using module (kinda global) level fields here. Go ahead, crucify me.
+
+        // Width/height of the canvas
+        var cWidth, cHeight;
+        // Needs a better name - the index (from the left) in 
+        var leftIndex = 0;
+
+        // The width, in pixels, of narrow & wide columns drawn onto this canvas.
+        // Should be rounded to the nearest int when set, depending on the size of the canvas.
+        var narrowWidth = 0;
+        var wideWidth = 0;
+
+        // The # of pixels from the left at which the drawing functions are pointing; this is
+        // where the next drawing operation will begin drawing a column. Updated whenever drawing
+        // occurs.
+        var curPixelIndex = 0;
+
+        // Clear the drawing area by overwriting it with a white rectangle
+        var clearCanvas = function(context) {
+            context.fillStyle = "white";
+            context.fillRect(0, 0, cWidth, cHeight);
+        };
+
+        // Draw the black/white bars for the given character
+        var drawCode = function(context, codeChar) {
+            // Get the sequence of wide, narrow, black, and white bar chars for this char
+            // Will be a sequence of B, b, W, & w, from the charTable
+            
+            // Fail if 
+            if (!charTable.hasOwnProperty(codeChar)) {
+                throw new Error("Unsupported character given to code39 writer: " + codeChar);
+                return;
+            }
+
+            var charBarSequence = charTable[codeChar];
+
+            // Draw a bar for each bar type character, advancing the curPixelIndex appropriately
+            for (var i = 0; i < charBarSequence.length; i++) {
+                drawBar(context, charBarSequence[i]);
+            };
+        };
+
+        // Draw the correct style of bar at the currentPixelIndex
+        var drawBar = function(context, typeChar) {
+            var fillCode = ""; // Black or white, set below
+            var fillWidth = 0; // The val of narrowWidth or wideWidth above, but set below
+
+            switch (typeChar) {
+                case "b":
+                    fillWidth = narrowWidth;
+                    fillCode = "black";
+                    break;
+                case "B":
+                    fillWidth = wideWidth;
+                    fillCode = "black";
+                    break;
+                case "w":
+                    fillWidth = narrowWidth;
+                    fillCode = "white";
+                    break;
+                case "W":
+                    fillWidth = wideWidth;
+                    fillCode = "white";
+                    break;
+                default:
+                    throw new Error("Invalid typeChar given to drawBar: " + typeChar);
+                    break;    
+            }
+
+            context.fillStyle = fillCode;
+            context.fillRect(curPixelIndex, 0, fillWidth, cHeight);
+
+            // Advance the curPixelIndex
+            curPixelIndex += fillWidth;
+        }
+
+        // Draw the code for a "*", the start and end code, meant to be placed at the beginning and
+        // end of the entire sequence.
+        var drawDelimeterCode = function(context) {
+            // Just a silly wrapper around drawCode()
+            drawCode(context, "*");
+        };
 
         // Get the canvas and it's 2d context
         // TODO: Put a check here to make sure 'this' is a jQuery result with a canvas at position '0'
@@ -188,11 +201,11 @@
 
         // Setup is complete, so draw one * code as the start sequence, then all of the dataString
         // chars, and then a final * code as the end sequence.
+        clearCanvas(context);
         drawDelimeterCode(context);
         drawBar(context, "w");
 
         for (var i = 0; i < dataString.length; i++) {
-            console.log(dataString[i]);
             drawCode(context, dataString[i]);
 
             if (i < dataString.length - 1) {
